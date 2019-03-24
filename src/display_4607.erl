e04755e129849d9364179e2e904e44c67aeb3354
Robert Forbes <robert.forbes@pentlandedge.com>
2019-03-24 22:56:15 +0000

    First version of the script that links in the decode 4607 step.

--- src/display_4607.erl
+++ src/display_4607.erl
@@ -9,9 +9,21 @@
 
 %% escript Entry point
 main(Args) ->
-    io:format("Args: ~p~n", [Args]),
-    erlang:halt(0).
+    try
+        [Filename] = Args,
+        Bin = s4607:read_file(Filename),
+        PacketList = s4607:decode(Bin),
+        s4607:display_packets(PacketList),
+        erlang:halt(0)
+    catch
+        _:_ ->
+            usage()
+    end.
 
 %%====================================================================
 %% Internal functions
 %%====================================================================
+
+usage() ->
+    io:format("usage: display_4607 filename\n"),
+    halt(1).
